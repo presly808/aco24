8793d50086b15c4840ea142d27f6f18971b677e7
vorobei <vorobei.yaroslav@gmail.com>
2018-04-01 11:07:56 +0300

    add new branch with intro task

--- YaroslavVorobei/src/main/java/intro/TextUtils.java
+++ YaroslavVorobei/src/main/java/intro/TextUtils.java
@@ -26,16 +26,32 @@ public class TextUtils {
      * return array with zero length
      */
     public static String[] getWords(String text, String delimeter) {
+       /* if(text == null || text.length()==0){
+            return null;
+        }*/
         return text.split(delimeter);
     }
 
     /**
      * @param words array with words
      * @return array with unique words
-     */
-    public static String[] getUniqueWords(String[] words) {
+     * /*
+     *//*
+  /*  public static String[] getUniqueWords(String[] words) {
         Set<String> set = new HashSet<String>(Arrays.asList(words));
         return set.toArray(new String[set.size()]);
+    }*/
+    public static String[] getUniqueWords(String[] words) {
+        String[] tempArr = new String[words.length];
+        int j = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (!hasElement(tempArr, words[i])) {
+                tempArr[j] = words[i];
+                j++;
+            }
+        }
+        String[] uniqWords = Arrays.copyOf(tempArr, j);
+        return uniqWords;
     }
 
     /**
@@ -45,5 +61,15 @@ public class TextUtils {
     public static String[] sortWords(String[] uniqueWords) {
         Arrays.sort(uniqueWords);
         return uniqueWords;
+    }
+
+
+    private static boolean hasElement(String[] words, String element) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] != null && words[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
