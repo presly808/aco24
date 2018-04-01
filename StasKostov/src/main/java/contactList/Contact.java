c3db835e7db77f4a32074ec7e7627763a09793f1
mac <mac@TX-N-0152.local>
2018-04-01 11:27:46 +0300

    - contact list

---
+++ StasKostov/src/main/java/contactList/Contact.java
@@ -0,0 +1,21 @@
+package contactList;
+
+/**
+ * Created by mac on 4/1/18.
+ */
+public class Contact {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String number;
+    private static int countId = 0;
+
+    public Contact( String firstName, String lastName, String number) {
+        id = countId++;
+        this.firstName = firstName;
+        this.number = number;
+        this.lastName = lastName;
+    }
+
+}
---
+++ StasKostov/src/main/java/contactList/ContactList.java
@@ -0,0 +1,25 @@
+package contactList;
+
+/**
+ * Created by mac on 4/1/18.
+ */
+public class ContactList {
+
+    public boolean addContact(Contact contact){
+        return false;
+    }
+
+    public Contact[] findByNameOrNumber(String nameOrNumber){
+        return null;
+    }
+
+    public boolean removeContact(int id){
+        return false;
+    }
+
+    public Contact[] getAll(){
+        return null;
+    }
+
+
+}
