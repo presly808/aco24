736c08cadcfc1859926293d6eff410885903123d
Valeriy Salmin <clickua8@gmail.com>
2018-04-17 12:45:10 +0300

    changes after review

--- ValeriiSalmin/src/main/java/week1/ContactList.java
+++ ValeriiSalmin/src/main/java/week1/ContactList.java
@@ -1,16 +1,21 @@
 package week1;
 
+import java.util.Arrays;
+
 /**
  * Created by serhii on 31.03.18.
  */
 public class ContactList {
 
-    Contact[] contactArray = new Contact[10];
-    int lastContactIndex = 0;
+    private Contact[] contactArray = new Contact[1];
+    private int lastContactIndex = 0;
+    private int phoneNumberSize = 13;
 
     public boolean addContact(Contact contact) {
         if (isValidContact(contact)) {
-            //TODO: need check/increase array
+            if (lastContactIndex == contactArray.length - 1) {
+                increaseArrayCapacity();
+            }
             contactArray[lastContactIndex] = contact;
             lastContactIndex++;
             return true;
@@ -21,45 +26,31 @@ public class ContactList {
     public Contact[] findByNameOrNumber(String nameOrNumber) {
         Contact[] contactsFoundTemp = new Contact[contactArray.length];
         int iPoint = 0;
-        for (int i = 0; i < contactArray.length; i++) {
+        for (int i = 0; i < lastContactIndex; i++) {
             if (contactArray[i] != null) {
                 if (contactArray[i].getNumber().contains(nameOrNumber) || contactArray[i].getName().contains(nameOrNumber)) {
-                    contactsFoundTemp[i] = contactArray[i];
+                    contactsFoundTemp[iPoint] = contactArray[i];
                     iPoint++;
                 }
             }
         }
-
-        Contact[] contactsFound = new Contact[iPoint];
-        for (int i = 0; i < contactsFound.length; i++) {
-            contactsFound[i] = contactsFoundTemp[i];
-        }
+        Contact[] contactsFound = Arrays.copyOf(contactsFoundTemp, iPoint);
         return contactsFound;
     }
 
     public boolean removeContact(int id) {
-        //TODO:
-        for (int i = 0; i < contactArray.length; i++) {
-            if (contactArray[i] != null) {
-                if (id == contactArray[i].getId()) {
-                    lastContactIndex--;
-                    contactArray[i] = null;
-                    return true;
-                }
+        for (int i = 0; i < lastContactIndex; i++) {
+            if (contactArray[i] != null && id == contactArray[i].getId()) {
+                contactArray[i] = null;
+                lastContactIndex--;
+                return true;
             }
         }
         return false;
     }
 
     public Contact[] getAll() {
-        Contact[] contactArrayTemp = new Contact[lastContactIndex];
-        for (int i = 0; i < contactArray.length; i++) {
-            for (int j = 0; j < lastContactIndex; j++) {
-                if ((contactArray[i] != null) && (contactArrayTemp[j] == null)) {
-                    contactArrayTemp[j] = contactArray[i];
-                }
-            }
-        }
+        Contact[] contactArrayTemp = Arrays.copyOf(contactArray, lastContactIndex);
         return contactArrayTemp;
     }
 
@@ -72,10 +63,15 @@ public class ContactList {
 
     public boolean isValidNumber(String number) {
         String regex = "\\d+";
-
-        if ((number.length() == 13) && (number.substring(1).matches(regex))) {
+        if ((number.length() == phoneNumberSize) && (number.substring(1).matches(regex))) {
             return true;
         }
         return false;
+    }
+
+    public void increaseArrayCapacity() {
+        Contact[] increasedContactsArray = new Contact[contactArray.length + 10];
+        System.arraycopy(contactArray, 0, increasedContactsArray, 0, contactArray.length);
+        contactArray = increasedContactsArray;
     }
 }
