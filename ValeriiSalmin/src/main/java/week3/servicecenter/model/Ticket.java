c5e264c222ee09b7d01e9a52d67223005a2316d0
valerii.sa <valerii.sa@sbtech.com>
2018-04-23 18:37:54 +0300

    add Item entity

--- ValeriiSalmin/src/main/java/week3/servicecenter/model/Ticket.java
+++ ValeriiSalmin/src/main/java/week3/servicecenter/model/Ticket.java
@@ -2,5 +2,14 @@ package week3.servicecenter.model;
 
 public class Ticket {
 
+    private Worker worker;
+    private User user;
+    private Item item;
+
+    private Ticket(Item item, User user){
+        this.item = item;
+        this.user = user;
+    }
+
 
 }
