146ceea8971fd0b939f50ce84d6938c401dafd56
Valeriy Salmin <clickua8@gmail.com>
2018-04-30 15:00:58 +0300

    save changes

--- ValeriiSalmin/src/main/java/week3/servicecenter/controller/LoginController.java
+++ ValeriiSalmin/src/main/java/week3/servicecenter/controller/LoginController.java
@@ -5,8 +5,14 @@ import week3.servicecenter.model.PersonDB;
 public class LoginController {
 
     PersonDB personDB;
+    String name;
+    String password;
 
-    public LoginController(PersonDB personDB) {
+    public LoginController(String name, String password) {
         this.personDB = personDB;
+        this.name = name;
+        this.password = password;
     }
+
+
 }
--- ValeriiSalmin/src/main/java/week3/servicecenter/controller/RunController.java
+++ ValeriiSalmin/src/main/java/week3/servicecenter/controller/RunController.java
@@ -2,10 +2,20 @@ package week3.servicecenter.controller;
 
 import week3.servicecenter.model.PersonDB;
 
+import java.util.Scanner;
+
 public class RunController {
 
     public static void main(String[] args) {
-        PersonDB personDB = new PersonDB();
-        LoginController loginController = new LoginController(personDB);
+        //PersonDB personDB = new PersonDB();
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Enter your password");
+        String password = scanner.nextLine();
+
+        LoginController loginController = new LoginController(name,password);
     }
 }
