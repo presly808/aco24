7a2771e8d600f3d713f84c8b8d664235b825afa8
Valeriy Salmin <clickua8@gmail.com>
2018-05-12 20:34:17 +0300

    IO utils

--- ValeriiSalmin/src/main/java/io/BashUtils.java
+++ ValeriiSalmin/src/main/java/io/BashUtils.java
@@ -34,28 +34,20 @@ public class BashUtils {
 
     public static boolean writeInto(String path, String src, boolean append)
             throws IOException {
-
-        File file = new File(path);
-        if (!file.exists()) {
-            file.createNewFile();
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(path,append)))) {
+            writer.write(src);
+            return true;
         }
-
-        try {
-            FileWriter fl = new FileWriter(path);
-            fl.write(src);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
     }
-
 
     public static List<String> ls(String path) throws FileNotFoundException {
         List<String> list = new ArrayList<>();
-        File dir = new File(System.getProperty(path));
-        for(File child: dir.listFiles()){
-            System.out.println(child);
-            list.add(child.getName());
+        String test = System.getProperty(path);
+        File dir = new File(test);
+        for(String child: dir.list()){
+            //System.out.println(child);
+            list.add(child);
         }
         return list;
     }
@@ -66,20 +58,6 @@ public class BashUtils {
         if (!file.exists()) {
             file.createNewFile();
         }
-//        BufferedReader fileReader = new BufferedReader(new FileReader(src));
-//        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dest));
-//        String line;
-//
-//        try {
-//            while ((line = fileReader.readLine()) != null) {
-//                fileWriter.write(line);
-//                return true;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-//        return false;
 
         try (
                 InputStream in = new BufferedInputStream(new FileInputStream(src));
@@ -97,14 +75,71 @@ public class BashUtils {
     }
 
     public static boolean move(String src, String dest) throws Exception {
+        File file = new File(dest);
+        File file1 = new File(src);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        try (
+                InputStream in = new BufferedInputStream(new FileInputStream(src));
+                OutputStream out = new BufferedOutputStream(new FileOutputStream(dest))) {
+
+            byte[] buffer = new byte[1024];
+            int lengthRead;
+            while ((lengthRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, lengthRead);
+                out.flush();
+                return true;
+            }
+            file1.delete();
+        }
         return false;
     }
 
     public static List<String> find(String path, String targetName) throws FileNotFoundException {
+        File directory = new File(path);
+        File[] fList = directory.listFiles();
         return null;
     }
 
     public static List<String> grep(String lines, String targetWord) {
+        double count = 0,countBuffer=0,countLine=0;
+        String lineNumber = "";
+        String filePath = lines;
+        BufferedReader br;
+        String inputSearch = "targetWord";
+        String line = "";
+        List<String> list = new ArrayList<>();
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            try {
+                while((line = br.readLine()) != null)
+                {
+                    countLine++;
+                    String[] words = line.split(" ");
+
+                    for (String word : words) {
+                        if (word.equals(inputSearch)) {
+                            count++;
+                            countBuffer++;
+                        }
+                    }
+                    if(countBuffer > 0)
+                    {
+                        countBuffer = 0;
+                        lineNumber += countLine + ",";
+                    }
+
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
