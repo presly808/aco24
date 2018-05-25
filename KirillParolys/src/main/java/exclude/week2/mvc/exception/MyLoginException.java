e85c5d47a0388bc533f88ec071c69988e74563dc
Kirill Parolys <kirillparolys@MacBook-Pro-Kirill.local>
2018-05-25 23:37:48 +0300

    Completed the server task

---
+++ KirillParolys/src/main/java/exclude/week2/mvc/exception/MyLoginException.java
@@ -0,0 +1,11 @@
+package exclude.week2.mvc.exception;
+
+/**
+ * Created by serhii on 13.05.18.
+ */
+public class MyLoginException extends Exception {
+
+    public MyLoginException(String message) {
+        super(message);
+    }
+}
